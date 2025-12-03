b2fb9bf77c53bc270e02bc972faffe02f524a406
Lucas Senno <senno.jpg@gmail.com>
2025-12-03 11:46:13 -0300

    update

--- dynff-master/test/Comp_FIM/generator/dummy.py
+++
@@ -1,8 +0,0 @@
-class DummyGenerator:
-
-    def __init__(self, layer_collection, device):
-        self.layer_collection = layer_collection
-        self.device = device
-
-    def get_device(self):
-        return self.device
--- dynff-master/test/gpu.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-import torch
-print("torch:", torch.__version__)
-print("torch cuda build:", torch.version.cuda)
-print("is_available:", torch.cuda.is_available())
